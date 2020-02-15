55a482a58d7c7532eff92ed71f7bc41efe8813fd
Joe Walbran <walbr037@morris.umn.edu>
2020-02-15 15:22:16 -0600

    Remove unused imports in tests
    Some tests don't use @Test; others don't use @ParameterizedTest. TodoControllerSpec doesn't use Validator or NotFoundResponse (yet).

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -14,18 +14,13 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 
-import io.javalin.core.validation.Validator;
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
-import io.javalin.http.NotFoundResponse;
-
-import umm3601.Server;
 
 /**
  * Tests the logic of the TodoController

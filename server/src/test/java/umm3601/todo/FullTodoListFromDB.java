d0b5f827f95a814437348d0474d38caa5b7a00f2
Joe Walbran <walbr037@morris.umn.edu>
2020-02-14 22:41:41 -0600

    Add another test for reading all todos.
    This test makes sure that the first todo read is also the first todo in
    the database.
    
    (This test makes the assumption that the database preserves the order of
    its content. Given that FullUserListFromDB makes the same assumption, I
    believe that this is a legitimate behavior to expect.)
    
    This commit addresses issue #9.

--- server/src/test/java/umm3601/todo/FullTodoListFromDB.java
+++ server/src/test/java/umm3601/todo/FullTodoListFromDB.java
@@ -30,4 +30,41 @@ public class FullTodoListFromDB {
         Arguments.of("/test-todos-2.json", 0),
         Arguments.of("/test-todos-3.json", 4));
   }
+
+  // This test makes the assumption that the database stores the
+  // todos in the same order that they appear in the json file.
+  // But this assumption is probably justified--FullUserListFromDB makes it as
+  // well.
+  @ParameterizedTest
+  @MethodSource("firstTodoInFullListParams")
+  public void firstTodoInFullList(
+      String dbFileName,
+      String firstTodoOwner,
+      boolean firstTodoStatus,
+      String firstTodoBody,
+      String firstTodoCategory) throws IOException {
+    Database db = new Database(dbFileName);
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+    Todo firstTodo = allTodos[0];
+    assertEquals(firstTodoOwner, firstTodo.owner, "Incorrect owner");
+    assertEquals(firstTodoStatus, firstTodo.status, "Incorrect status");
+    assertEquals(firstTodoBody, firstTodo.body, "Incorrect body");
+    assertEquals(firstTodoCategory, firstTodo.category, "Incorrect category");
+  }
+
+  public Stream<Arguments> firstTodoInFullListParams() {
+    return Stream.of(
+        Arguments.of(
+            "/test-todos-1.json",
+            "Kang",
+            true,
+            "Quae res in civitate duae plurimum possunt, eae contra nos ambae faciunt in hoc tempore, summa gratia et eloquentia; quarum alteram, C. Aquili, vereor, alteram metuo.",
+            "sales"),
+        Arguments.of(
+            "/test-todos-3.json",
+            "",
+            false,
+            "<script>alert('Problem!');</script>",
+            "😃"));
+  }
 }
